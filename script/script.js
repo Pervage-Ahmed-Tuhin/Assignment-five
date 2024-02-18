acfb81ebe890d4d0323a1fb0c575e81d8c7d2b02
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-02-18 22:35:36 +0600

    upadted some stuff

--- script/script.js
+++ script/script.js
@@ -12,6 +12,33 @@ function innerSection(){
     value4.classList.remove('hidden');
 }
 
+// This function hides the inner part and shows the starting part
+function modalContinue(){
+
+    // const value = document.getElementById('modal-continue-btn');
+
+    
+    const value = document.getElementById('banner-section');
+    value.classList.remove('hidden');
+    const value2 = document.getElementById('offer-section');
+    value2.classList.remove('hidden');
+    const value3 = document.getElementById('upper1');
+    value3.classList.add('hidden');
+    const value4 = document.getElementById('upper2');
+    value4.classList.add('hidden');
+
+}
+
+//This function hides everything accepts the modal section
+
+
+
+   
+  
+  
+
+
+
 
 
 
