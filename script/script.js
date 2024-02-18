145c0db8b98d9d9fdd696703f7e10194e106ce33
Pervage-Ahmed-Tuhin <parvageahmedtuhin123456@gmail.com>
2024-02-18 22:09:36 +0600

    Added banner and modal java script

---
+++ script/script.js
@@ -0,0 +1,17 @@
+
+
+
+function innerSection(){
+    const value2 = document.getElementById('offer-section');
+    value2.classList.add('hidden');
+    const value = document.getElementById('banner-section');
+    value.classList.add('hidden');
+    const value3 = document.getElementById('upper1');
+    value3.classList.remove('hidden');
+    const value4 = document.getElementById('upper2');
+    value4.classList.remove('hidden');
+}
+
+
+
+
